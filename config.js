c8ba4b45b274f69c09b98acd35cbce78d0d36260
antonioGB08 <128941157+antonioGB08@users.noreply.github.com>
2023-03-26 01:43:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,12 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['393312171655', '𝐄ИΞM𝕀Ξ𝐒 𝐁Ꮻ𝐓', true],
-  ['377607935810'],
-  ['447700080855'],
-  //['393312171655'],	
-  ['393312171655']
+  [ '68987330000', 'ombra', true],
+  ['16095663280']
 ] 
 
-global.suittag = ['393312171655'] 
-global.prems = ['393312171655'] 
+global.suittag = ['68987330000'] 
+global.prems = ['68987330000'] 
 
 global.packname = '𝐄ИΞM𝕀Ξ𝐒'
 global.author = '𝐁Ꮻ𝐓'
